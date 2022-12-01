00daeb6c5025e871ab5e02ab20fe0c3e6d8db564
Md-Rizwan <rizwan@saglus.com>
2022-12-02 01:31:18 +0530

    new work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './public/**/*.{html,js}',
+    './public/**/*.{html,js}',
+  ],
   theme: {
     extend: {},
   },
